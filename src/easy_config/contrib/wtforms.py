ccba94911f99fb1fb26627d98feff0b6952dfe71
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-09-29 21:17:18 +0200

    Improve extensibility
    - wtform.Form by default instead of flask_wtf.FlaskForm
    - make base Form class a paremeter
    - more type annotations
    - more scalar form field definitions
    - support for field list

--- src/easy_config/contrib/wtforms.py
+++ src/easy_config/contrib/wtforms.py
@@ -1,29 +1,48 @@
-"""Test the Flask-WTF wrapper."""
+# -*- coding: utf-8 -*-
+
+"""A wrapper for generating forms with a :mod:`wtforms`."""
 
 import dataclasses
-from typing import Type
+from datetime import date, datetime
+from typing import List, Type
+
+from wtforms import BooleanField, DateField, DateTimeField, FieldList, FloatField, Form, IntegerField, StringField
+from wtforms.validators import DataRequired
 
 from easy_config import EasyConfig
-from flask_wtf import FlaskForm
-from wtforms import BooleanField, IntegerField, StringField
-from wtforms.validators import DataRequired
+
+__all__ = [
+    'form_from_config',
+]
 
 type_to_field = {
     str: StringField,
     int: IntegerField,
     bool: BooleanField,
+    float: FloatField,
+    date: DateField,
+    datetime: DateTimeField,
 }
 
 
-def form_from_config(cls: Type[EasyConfig]):
-    """Build a Flask-WTF form based on the given EasyConfig class."""
+def form_from_config(cls: Type[EasyConfig], base: Type[Form] = Form) -> Type[Form]:
+    """Build a Flask-WTF form based on the given EasyConfig class.
+
+    `base` can be replaced with :class:`flask_wtf.FlaskForm` as well.
+    """
     attrs = {}
 
-    for field in dataclasses.fields(cls):
-        field_cls = type_to_field[field.type]
-        attribute = field_cls(field.name, validators=[DataRequired()])
-        if field.default is not dataclasses.MISSING:
-            attribute.default = field.default
-        attrs[field.name] = attribute
+    for dataclass_field in dataclasses.fields(cls):
+        if isinstance(dataclass_field.type, List.__class__):
+            arg = dataclass_field.type.__args__[0]
+            form_field_cls = type_to_field[arg]
+            form_field = FieldList(form_field_cls(dataclass_field.name, validators=[DataRequired()]))
+        else:
+            form_field_cls = type_to_field[dataclass_field.type]
+            form_field = form_field_cls(dataclass_field.name, validators=[DataRequired()])
 
-    return type(f'{cls.NAME}Form', (FlaskForm,), attrs)
+        if dataclass_field.default is not dataclasses.MISSING:
+            form_field.default = dataclass_field.default
+        attrs[dataclass_field.name] = form_field
+
+    return type(f'{cls.NAME}Form', (base,), attrs)
--- tests/test_contrib_wtforms.py
+++ tests/test_contrib_wtforms.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
-"""Test the Flask-WTF wrapper."""
+"""Test the :mod:`wtforms` wrapper."""
 
 import flask
+from flask_wtf import FlaskForm
 
 from easy_config import EasyConfig
 from easy_config.contrib.wtforms import form_from_config
 
 
-def test_with_default():
+def test_with_default():  # noqa: D202
     """Test building a form."""
 
     class ExampleConfig(EasyConfig):
@@ -17,19 +18,20 @@ def test_with_default():
         FILES = None
         NAME = 'MyProgram'
 
+        number: int
         bool_1: bool
-        bool_2: bool = True
+        bool_2: bool = False
         bool_3: bool = True
 
-    form_cls = form_from_config(ExampleConfig)
+    form_cls = form_from_config(ExampleConfig, base=FlaskForm)
+    form_cls.Meta.csrf = False
     app = flask.Flask('test_app')
 
     with app.app_context():
-        form = form_cls(csrf_enabled=False)
-        for field in form:
-            print(field)
-
+        form = form_cls()
         fields = list(form)
-        assert 1 == len(fields)
-        field = fields[0]
-        assert str(field) == '<input id="number" name="number" required type="checkbox" value="y">'
+        assert 4 == len(fields)
+        assert str(fields[0]) == '<input id="number" name="number" required type="text" value="">'
+        assert str(fields[1]) == '<input id="bool_1" name="bool_1" required type="checkbox" value="n">'
+        assert str(fields[2]) == '<input id="bool_2" name="bool_2" required type="checkbox" value="n">'
+        assert str(fields[3]) == '<input id="bool_3" name="bool_3" required type="checkbox" value="y">'
